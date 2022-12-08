73cfef5390e536ed57e7496b52469ede6a1cd4b7
KenanMehmet <kenmehmet1@gmail.com>
2022-12-08 13:11:23 +0000

    read the file and splitted the lines

--- Day1/main.py
+++ Day1/main.py
@@ -1,3 +1,5 @@
+import os
 
 if __name__ == "__main__":
-    print("Hello World")
+    file = open(r"C:\Users\Kenan\programming\advent-of-code-2022\day1\puzzle test file.txt")
+    print(file.read().splitlines())
