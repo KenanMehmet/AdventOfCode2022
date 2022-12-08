4794099d43493286881be7dec76d9d192cb2d43a
KenanMehmet <kenmehmet1@gmail.com>
2022-12-08 18:30:21 +0000

    finished sumScore function

--- day2/puzzle_1/functions.py
+++ day2/puzzle_1/functions.py
@@ -1,12 +1,15 @@
 beats = {"A": "Z", "B": "X", "C": "Y"}
 conversion = {"A": "X", "B": "Y", "C": "Z"}
+choice_points = {"X": 1, "Y": 2, "Z": 3}
 
 def sumScore(file):
+    score = 0
     for line in file:
-        print(line)
+        score += choice_points[line[2]] 
         if line[2] == beats[line[0]]:
-            print("win")
-        elif conversion[0] == line[2]:
-            print("tie")
+            continue
+        elif conversion[line[0]] == line[2]:
+            score += 3
         else:
-            print("loss")
+            score += 6
+    return score
