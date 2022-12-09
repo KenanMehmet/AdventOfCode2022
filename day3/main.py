77746a7719b0e6f8df7de12ba4d6afe7e855110d
KenanMehmet <kenmehmet1@gmail.com>
2022-12-09 15:07:27 +0000

    correct answer

--- day3/main.py
+++ day3/main.py
@@ -4,7 +4,7 @@ from puzzle_2.functions import findBadge, splitGroups
 
 if __name__ == "__main__":
     """ Find the elf holding the most snacks in calories"""
-    file = Path(__file__).with_name('puzzle .txt')
+    file = Path(__file__).with_name('puzzle input.txt')
     with file.open('r') as f:
         #print(findRepeat(f))
         print(splitGroups(f))
