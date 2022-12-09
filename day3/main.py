ee3b913807a9601c5382705bb31d9d4028d79696
KenanMehmet <kenmehmet1@gmail.com>
2022-12-09 14:42:22 +0000

    fixed subtraction values based on ord

--- day3/main.py
+++ day3/main.py
@@ -3,6 +3,6 @@ from puzzle_1.functions import findRepeat
 
 if __name__ == "__main__":
     """ Find the elf holding the most snacks in calories"""
-    file = Path(__file__).with_name('puzzle input.txt')
+    file = Path(__file__).with_name('puzzle test.txt')
     with file.open('r') as f:
         print(findRepeat(f))
--- day3/puzzle_1/functions.py
+++ day3/puzzle_1/functions.py
@@ -1,16 +1,24 @@
 
 def findRepeat(file):
-    repeats_value = 0
+    #repeats_value = 0
+    repeats = []
     for line in file:
         half_point = int(len(line.strip()) / 2)
         first_compartment = line[0:half_point]
         second_compartment = line[half_point:]
         for char in first_compartment:
             if char in second_compartment:
-                repeats_value += getCharValue(char)
-    return repeats_value
+                print(char)
+                print(second_compartment)
+                #repeats_value += getCharValue(char)
+                repeats.append([char, getCharValue(char)])
+                break
+    print(repeats)
+    #return repeats_value
 
 def getCharValue(char):
+    print(char)
+    print(ord(char))
     if char.islower():
-        return ord(char) - 70
-    return ord(char) - 64
+        return ord(char) - 96
+    return ord(char) - 38
