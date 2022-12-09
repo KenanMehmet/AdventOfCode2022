308af729b66d0389fdf42257986489e90b3c831c
KenanMehmet <kenmehmet1@gmail.com>
2022-12-09 14:43:06 +0000

    removed testing code, correct answer acheived

--- day3/puzzle_1/functions.py
+++ day3/puzzle_1/functions.py
@@ -1,24 +1,18 @@
 
 def findRepeat(file):
-    #repeats_value = 0
-    repeats = []
+    repeats_value = 0
     for line in file:
         half_point = int(len(line.strip()) / 2)
         first_compartment = line[0:half_point]
         second_compartment = line[half_point:]
         for char in first_compartment:
             if char in second_compartment:
-                print(char)
-                print(second_compartment)
-                #repeats_value += getCharValue(char)
-                repeats.append([char, getCharValue(char)])
+
+                repeats_value += getCharValue(char)
                 break
-    print(repeats)
-    #return repeats_value
+    return repeats_value
 
 def getCharValue(char):
-    print(char)
-    print(ord(char))
     if char.islower():
         return ord(char) - 96
     return ord(char) - 38
