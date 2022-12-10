19e0aaed927399b5fa0a5c72d1d2be1008e08f73
KenanMehmet <kenmehmet1@gmail.com>
2022-12-10 17:40:25 +0000

    split the nested for loops and functions, testing code

--- day5/main.py
+++ day5/main.py
@@ -2,19 +2,11 @@ from pathlib import Path
 
 def runCraneOperations(file):
     """Here we will loop through the lines of the file and run the correct functions"""
-    stacks = [
-        ['[Z]', '[N]'],
-        ['[M]', '[C]', '[D]'],
-        ['[P]']
-    ]
+    stacks = []
     stacks_made = False
     for line in file.readlines():
-        print(line)
         if not stacks_made:
-            line = line[0:-1] #Cull the /n but keep the spaces
-            #stacks.append([line[n: n + 4] for n in range(0, len(line), 4)])
-            if not line.strip():
-                stacks_made = True
+            stacks_made = createStacksArray(stacks, line)
         else:
             moveCrates(line, stacks)
     print(stacks)
@@ -22,26 +14,21 @@ def runCraneOperations(file):
 
 def moveCrates(line, stacks):
     """Follow Move instructions"""
-    print("MOVING CRATES")
-    print(line)
-    print(stacks)
     line = line.split()
     origin_stack = stacks[int(line[3]) - 1]
     new_stack = stacks[int(line[5]) - 1]
     for i in range(int(line[1])):
-        print(origin_stack)
-        print(new_stack)
-        print(origin_stack[-1])
         new_stack.append(origin_stack[-1])
         origin_stack.pop()
-        print(new_stack)
-        print(origin_stack)
-    print(stacks)
-    print("DONE MOVING")
 
 def createStacksArray(stacks, line):
-    
     """In the text file, the crates stacks are seperated by an empty line"""
+    if not line.strip():
+        stacks = [stack.reverse() for stack in stacks]
+        return True
+    line = line[0:-1] #Cull the /n but keep the spaces
+    stacks.append([line[n: n + 4] for n in range(0, len(line), 4)])
+    return True
 
 
 if __name__ == "__main__":
