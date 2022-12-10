85a917dc0c421ce19137c943f5d399b498d3f31c
KenanMehmet <kenmehmet1@gmail.com>
2022-12-10 21:53:58 +0000

    started inital setup for mapping out directory tree, currently deciding whether to use a string counting method instead of mapping out for simplicity

--- day7/main.py
+++ day7/main.py
@@ -1,20 +1,47 @@
 from pathlib import Path
-Tree = {}
+file_tree = {
+    "/": {
+        "child": {},
+        "size": 0
+    }
+} #Key will be string, have two values, child directories and size of all items
 
 def runCommands(file):
-    
+    current_directory = ""
     for line in file.readlines():
-        print(line.strip())
+        print(current_directory)
+        current_directory = interpretLine(line.strip(), current_directory)
+        print(file_tree)
 
-def interpretLine(line):
+def interpretLine(line, current_directory):
+    print(line)
     if line[0] == "$":
+        command = line[2:]
+        if command[0] == "c":
+            return runChangeDirectory(command[3:], current_directory)
+        else:
+            runList(command, current_directory)
+    else:
+        if line[0] == "d":
+            #file_tree[current_directory]
+            print("not yet")
+        else:
+            line = line.split()
+            file_tree[current_directory]["size"] += int(line[0])
+    return current_directory
+
+def runChangeDirectory(line, current_directory):
+    if line != "..":
+        return current_directory + line
+    if len(current_directory.split()) == 1:
+        return "/"
+    current_directory.split().pop()
+    return ''.join(current_directory)
+    
+
+def runList(line, current_directory):
+    if current_directory == "":
         return True
-
-def runChangeDirectory(line):
-    return True
-
-def runList(line):
-    return True
 
 if __name__ == "__main__":
     """ 
