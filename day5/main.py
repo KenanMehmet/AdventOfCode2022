9a1887649154fc83d4b7022e1fa16138d6d92387
KenanMehmet <kenmehmet1@gmail.com>
2022-12-10 17:34:12 +0000

    finished move crates function

--- day5/main.py
+++ day5/main.py
@@ -2,17 +2,45 @@ from pathlib import Path
 
 def runCraneOperations(file):
     """Here we will loop through the lines of the file and run the correct functions"""
-    stacks = []
-    for line in file:
-        print(line.strip().split(' '))
-        if not line.strip():
-            test_stack = [line[n: n + 3] for n in range(0, len(line), 5)]
-            print(test_stack)
+    stacks = [
+        ['[Z]', '[N]'],
+        ['[M]', '[C]', '[D]'],
+        ['[P]']
+    ]
+    stacks_made = False
+    for line in file.readlines():
+        print(line)
+        if not stacks_made:
+            line = line[0:-1] #Cull the /n but keep the spaces
+            #stacks.append([line[n: n + 4] for n in range(0, len(line), 4)])
+            if not line.strip():
+                stacks_made = True
+        else:
+            moveCrates(line, stacks)
+    print(stacks)
 
-def moveCrates(file):
+
+def moveCrates(line, stacks):
     """Follow Move instructions"""
+    print("MOVING CRATES")
+    print(line)
+    print(stacks)
+    line = line.split()
+    origin_stack = stacks[int(line[3]) - 1]
+    new_stack = stacks[int(line[5]) - 1]
+    for i in range(int(line[1])):
+        print(origin_stack)
+        print(new_stack)
+        print(origin_stack[-1])
+        new_stack.append(origin_stack[-1])
+        origin_stack.pop()
+        print(new_stack)
+        print(origin_stack)
+    print(stacks)
+    print("DONE MOVING")
 
-def createStacksArray(file):
+def createStacksArray(stacks, line):
+    
     """In the text file, the crates stacks are seperated by an empty line"""
 
 
@@ -25,4 +53,4 @@ if __name__ == "__main__":
     """
     file = Path(__file__).with_name('puzzle test.txt')
     with file.open('r') as f:
-        print(moveCrates(f))
+        print(runCraneOperations(f))
