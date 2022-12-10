6f945f65128da17ce0fb101348d0dad1fb59cc4c
KenanMehmet <kenmehmet1@gmail.com>
2022-12-10 18:06:07 +0000

    started creation of puzzle 2 function

--- day5/main.py
+++ day5/main.py
@@ -8,7 +8,7 @@ def runCraneOperations(file):
         if not stacks_made:
             stacks, stacks_made = createStacksArray(stacks, line)
         else:
-            moveCrates(line, stacks)
+            moveAllCrates(line, stacks)
     top_crates = [stack[-1][1] for stack in stacks]
     return ''.join(top_crates)
 
@@ -37,12 +37,25 @@ def createStacksArray(stacks, line):
             stacks[i].append(line_splited[i])
     return [stacks, False]
 
+def moveAllCrates(line, stacks):
+    """Follow Move instructions"""
+    line = line.split()
+    origin_stack = stacks[int(line[3]) - 1]
+    new_stack = stacks[int(line[5]) - 1]
+    crates = origin_stack[-int(line[1]):]
+    print(crates)
+    for i in range(int(line[1])):
+        new_stack.append(origin_stack[-1])
+        origin_stack.pop()
+
 if __name__ == "__main__":
     """ 
         Puzzle 1: Supplies are stored in stacked crates, but the most needed crates are buried, we need to move them
         the crane operator who moves them does them in one go, she does them in these steps
         move 1 from 2 to 1: move one object from stack 2 to stack 1
         After all the instructions, list the top crate of each stack.
+
+        Puzzle 2: The Crane moves all crates in one go, update code accordingly
     """
     file = Path(__file__).with_name('puzzle test.txt')
     with file.open('r') as f:
